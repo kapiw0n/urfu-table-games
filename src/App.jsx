7b6824d9b45b8ed948c6f8b55b9b69dd22494cdc
Kapiw0n <flatline11@mail.ru>
2025-04-29 01:22:06 +0500

    semi-stilyzed auth page

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,24 @@ export default function App() {
         <div className = "auth-panel">
 
           <div className = "auth-tabs">
-            <a className= "auth-tab" href = "#"><p>вход</p></a>
-            <a className= "auth-tab" href = "#"><p>регистрация</p></a>
+            <a className= "auth-tab" href = "#">вход</a>
+            <a className= "auth-tab" href = "#">регистрация</a>
           </div>
 
           <div className='auth-inputs'>
             <input type='e-mail' placeholder='E-mail'></input>
             <input type='password' placeholder='Пароль'></input>
-            
-            <div className=''><input type="checkbox"/><span className='auth-remember'>Запомнить меня</span></div>
-
-            <button type = "submit">Войти</button>
           </div>
           
-          <div className='auth-decorline'></div>
-          
-          <a className = "auth-restore-password" href = "#"><p>Забыли пароль?</p></a>
-          <button type="submit" className='auth-google-button'>Продолжить с <b>Google</b></button>
+          <div className='auth-rememberbox'><input className = "auth-checkbox" type= "checkbox"/><span className='auth-remember'>Запомнить меня</span></div>
 
+          <div className='auth-buttons'>
+            <div className='auth-decorline'>
+              <a href = "#" className=''><button className = "auth-enter" type = "submit">Войти</button></a>
+            </div>
+            <a className = "auth-restore-password" href = "#"><p>Забыли пароль?</p></a>
+            <button type="submit" className='auth-google-button'>Продолжить с <b>Google</b></button>
+          </div>
         </div>
       </div>
     </>
