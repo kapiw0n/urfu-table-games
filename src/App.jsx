df218cc5137daa32aa5e265fa5acc653b9278e61
Kapiw0n <flatline11@mail.ru>
2025-04-29 13:24:05 +0500

    full style for auth page

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,21 @@ export default function App() {
         <h1 className = "auth-heading">Настольные игры УрФУ</h1>
 
         <div className = "auth-panel">
-
+          
           <div className = "auth-tabs">
             <a className= "auth-tab" href = "#">вход</a>
             <a className= "auth-tab" href = "#">регистрация</a>
           </div>
 
           <div className='auth-inputs'>
-            <input type='e-mail' placeholder='E-mail'></input>
+            <div className='auth-input-container'>
+              <input type='e-mail' placeholder='E-mail'></input>
+              <img className='auth-mail-icon' src = "src/assets/img/email-icon.svg"></img>
+            </div>
+            <div className='auth-input-container'>
             <input type='password' placeholder='Пароль'></input>
+            <img className='auth-password-icon' src = "src/assets/img/password-icon.svg"></img>
+            </div>
           </div>
           
           <div className='auth-rememberbox'><input className = "auth-checkbox" type= "checkbox"/><span className='auth-remember'>Запомнить меня</span></div>
@@ -29,7 +35,12 @@ export default function App() {
               <a href = "#" className=''><button className = "auth-enter" type = "submit">Войти</button></a>
             </div>
             <a className = "auth-restore-password" href = "#"><p>Забыли пароль?</p></a>
-            <button type="submit" className='auth-google-button'>Продолжить с <b>Google</b></button>
+
+            <a href = "#"><button type="submit" className='auth-google-button'>
+            <img className='auth-google-icon' src = "src/assets/img/google-icon.png"></img>
+            Продолжить с <b>Google</b>
+            </button></a>
+
           </div>
         </div>
       </div>
